3a12dadf1ceae6bb264565ac5f876058b76c2f71
Shaikh Shadul <sshadu@codeaurora.org>
2017-05-02 18:24:18 +0530

    Sensors: Move sensors HAL library to vendor
    Update makefile to place sensors hal library part of vendor
    instead of system.
    
    Change-Id: I23762221510b916b3cdf6e014bbae392e88aaa83

--- Android.mk
+++ Android.mk
@@ -10,6 +10,8 @@ include $(CLEAR_VARS)
 
 ifeq ($(USE_SENSOR_MULTI_HAL),true)
   LOCAL_MODULE := sensors.native
+  LOCAL_PROPRIETARY_MODULE := true
+  LOCAL_VENDOR_MODULE := true
 else
   ifneq ($(filter msm8610,$(TARGET_BOARD_PLATFORM)),)
     LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
